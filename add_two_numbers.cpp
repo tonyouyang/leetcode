51ade4b5701bb7e8fd73f90bfcfa7329a20dc21f
tony <oy@lianlianke.com>
2013-09-06 22:42:02 +0800

    Add two numbers AC

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -19,70 +19,99 @@ struct ListNode
 class Solution
 {
 public:
-    int listToNum(ListNode *list)
-    {
-        int r = 0, d = 1;
+    // int listToNum(ListNode *list)
+    // {
+    //     int r = 0, d = 1;
 
-        while (list != NULL)
-        {
-            r += (list->val) * d;
-            d *= 10;
-            list = list->next;
-        }
+    //     while (list != NULL)
+    //     {
+    //         r += (list->val) * d;
+    //         d *= 10;
+    //         list = list->next;
+    //     }
 
-        return r;
-    }
+    //     return r;
+    // }
 
-    ListNode *numToList(int x)
-    {
-        if (!x)
-        {
-            return new struct ListNode(x);
-        }
-        else
-        {
-            ListNode *head = NULL, *curr = NULL;
+    // ListNode *numToList(int x)
+    // {
+    //     if (!x)
+    //     {
+    //         return new struct ListNode(x);
+    //     }
+    //     else
+    //     {
+    //         ListNode *head = NULL, *curr = NULL;
 
-            while (x)
-            {
-                if (curr != NULL)
-                {
-                    curr->next = new struct ListNode(x % 10);
-                    curr = curr->next;
-                }
-                else
-                {
-                    curr = new struct ListNode(x % 10);
-                    head = curr;
-                }
-                x /= 10;
-            }
+    //         while (x)
+    //         {
+    //             if (curr != NULL)
+    //             {
+    //                 curr->next = new struct ListNode(x % 10);
+    //                 curr = curr->next;
+    //             }
+    //             else
+    //             {
+    //                 curr = new struct ListNode(x % 10);
+    //                 head = curr;
+    //             }
+    //             x /= 10;
+    //         }
 
-            curr = NULL;
+    //         curr = NULL;
 
-            return head;
-        }
-    }
+    //         return head;
+    //     }
+    // }
 
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
     {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        int n1 = listToNum(l1), n2 = listToNum(l2);
-        // cout << n1 << ',' << n2 << endl;
-        return numToList(n1 + n2);
+        ListNode *head = NULL, *curr = NULL;
+        int m = 0, n;
+
+        while (l1 != NULL || l2 != NULL || m > 0)
+        {
+            n = m;
+            if (l1 != NULL)
+            {
+                n += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL)
+            {
+                n += l2->val;
+                l2 = l2->next;
+            }
+            m = n / 10;
+            n %= 10;
+
+            if (curr != NULL)
+            {
+                curr->next = new struct ListNode(n);
+                curr = curr->next;
+            }
+            else
+            {
+                curr = new struct ListNode(n);
+                head = curr;
+            }
+        }
+
+        return head;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-    Solution solution;
-    int n1, n2;
+    // Solution solution;
+    // int n1, n2;
 
-    cin >> n1 >> n2;
-    ListNode *l1 = solution.numToList(n1);
-    ListNode *l2 = solution.numToList(n2);
-    cout << solution.listToNum(solution.addTwoNumbers(l1, l2)) << endl;
+    // cin >> n1 >> n2;
+    // ListNode *l1 = solution.numToList(n1);
+    // ListNode *l2 = solution.numToList(n2);
+    // cout << solution.listToNum(solution.addTwoNumbers(l1, l2)) << endl;
 
     return 0;
 }
