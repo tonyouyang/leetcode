668dda0ec7fd364276c63144e4444b361450e3dd
tony <oy@lianlianke.com>
2013-09-11 20:07:04 +0800

    Binary tree level order traversal AC.

---
+++ binary_tree_level_order_traversal.cpp
@@ -0,0 +1,78 @@
+// Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+// For example:
+// Given binary tree {3,9,20,#,#,15,7},
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its level order traversal as:
+
+// [
+//   [3],
+//   [9,20],
+//   [15,7]
+// ]
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    void f(TreeNode *root, const int d, vector<vector<int> > &v)
+    {
+        if (v.size() < d + 1)
+        {
+            vector<int> nv(1, root->val);
+            v.push_back(nv);
+        }
+        else
+        {
+            v[d].push_back(root->val);
+        }
+
+        if (root->left != NULL) f(root->left, d + 1, v);
+        if (root->right != NULL) f(root->right, d + 1, v);
+    }
+
+    vector<vector<int> > levelOrder(TreeNode *root)
+    {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<vector<int> > v;
+
+        if (root == NULL) return v;
+        f(root, 0, v);
+        return v;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution solution;
+    TreeNode *t0 = new TreeNode(0), *t1 = new TreeNode(1), *t2 = new TreeNode(2);
+
+    t0->left = t1;
+    t0->right = t2;
+
+    vector<vector<int> > v = solution.levelOrder(t0);
+    for (int i = 0, p = v.size(); i < p; ++i)
+    {
+        for (int j = 0, q = v[i].size(); j < q;  ++j) cout << v[i][j] << " ";
+        cout << endl;
+    }
+    
+    return 0;
+}
