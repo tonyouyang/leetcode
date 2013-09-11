78e595331497fa3b7849155a7d008d0485570702
tony <oy@lianlianke.com>
2013-09-12 02:02:06 +0800

    minor refine

--- interleaving_string.cpp
+++ interleaving_string.cpp
@@ -27,6 +27,11 @@ public:
         if (s1_len + s2_len != s3_len) return false;
         if (s1_len == 0 && s2_len == 0) return true;
 
+        if (s1_len > s2_len) {
+            swap(s1, s2);
+            swap(s1_len, s2_len);
+        }
+
         bool v[2][s1_len + 1];
         bool ret;
         memset(v, false, sizeof(v));
@@ -56,11 +61,13 @@ int main(int argc, char const *argv[])
     string a1("abc"), b1("def"), c1("abfedc"); // false
     string a2("aabcc"), b2("dbbca"), c2("aadbbcbcac"); // true
     string a3(""), b3(""), c3(""); // true
+    string a4("aaa"), b4("bb"), c4("abbaa"); // true
 
     cout << a0 << " + " << b0 << " = " << c0 << " ? " << solution.isInterleave(a0, b0, c0) << endl;
     cout << a1 << " + " << b1 << " = " << c1 << " ? " << solution.isInterleave(a1, b1, c1) << endl;
     cout << a2 << " + " << b2 << " = " << c2 << " ? " << solution.isInterleave(a2, b2, c2) << endl;
     cout << a3 << " + " << b3 << " = " << c3 << " ? " << solution.isInterleave(a3, b3, c3) << endl;
+    cout << a4 << " + " << b4 << " = " << c4 << " ? " << solution.isInterleave(a4, b4, c4) << endl;
 
     return 0;
 }
