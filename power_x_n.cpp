987f467b291dfd0efa98f1687c13b2c9286bbeed
tony <oy@lianlianke.com>
2013-09-13 00:05:54 +0800

    clearer code

--- power_x_n.cpp
+++ power_x_n.cpp
@@ -13,24 +13,18 @@ public:
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         if (n == 0) return 1;
-        if (n == 1) return x;
-        if (x < 1e-8 && x > -1e-8) return n > 0 ? 0 : numeric_limits<double>::max();
         if (x < 1 + 1e-8 && x > 1 - 1e-8) return 1;
         if (x < -1 + 1e-8 && x > -1 - 1e-8) return n & 1 ? -1 : 1;
-        if (n == -1) return 1 / x;
-
+        
         int m = abs(n);
-        double p = m & 1 ? x : 1, t = x;
+        double p = 1;
 
-        do
+        for ( ; m; x *= x, m >>= 1)
         {
-            m = m >> 1;
-            t *= t;
-            if (m & 1) p *= t;
+            if (m & 1) p *= x;
         }
-        while (m);
 
-        return n > 0 ? p : 1 / p;
+        return n < 0 ? 1 / p : p;
     }
 };
 
@@ -53,7 +47,7 @@ int main(int argc, char const *argv[])
     cout << solution.pow(-10, -1) << endl;
     cout << solution.pow(1, -1) << endl;
     cout << solution.pow(-1, -1) << endl;
-    cout << solution.pow(0, -1) << endl;
+    // cout << solution.pow(0, -1) << endl;
 
     return 0;
 }
