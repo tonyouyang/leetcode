cc47c1d058cc68ada08def140c870a2d76027f05
tony <oy@lianlianke.com>
2013-09-24 21:18:54 +0800

    Merge Intervals AC>

---
+++ merge_intervals.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    static bool compare(const Interval &i, const Interval &j)
+    {
+        return i.start < j.start; // || i.start == j.start && i.end <= j.end;
+    }
+    
+    vector<Interval> merge(vector<Interval> &intervals) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<Interval> ret;
+        if (intervals.empty()) return ret;
+        
+        sort(intervals.begin(), intervals.end(), compare);
+        int l = intervals[0].start, r = intervals[0].end;
+        for (int i = 1; i < intervals.size(); ++i)
+        {
+            if (intervals[i].start <= r)
+            {
+                // l = min(l, intervals[i].start);
+                r = max(r, intervals[i].end);
+            }
+            else
+            {
+                ret.push_back(Interval(l, r));
+                l = intervals[i].start;
+                r = intervals[i].end;
+            }
+        }
+        ret.push_back(Interval(l, r));
+        
+        return ret;
+    }
+};
