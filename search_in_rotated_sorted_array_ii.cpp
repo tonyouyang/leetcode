a4cc1b11cb458062de0a61db2182119acc82a759
tony <oy@lianlianke.com>
2013-09-24 20:00:03 +0800

    Search in Rotated Sorted Array II AC.

---
+++ search_in_rotated_sorted_array_ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool search(int A[], int n, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int l = 0, r = n - 1, m;
+        
+        while (l <= r)
+        {
+            m = l + (r - l >> 1);
+            if (A[m] == target)  return true;
+            if (A[m] > A[l])
+            {
+                if (A[l] <= target && target < A[m]) r = m - 1;
+                else l = m + 1;
+            }
+            else if (A[m] < A[l])
+            {
+                if (A[m] < target && target <= A[r]) l = m + 1;
+                else r = m - 1;
+            }
+            else
+            {
+                l++; // skip one duplicate
+            }
+        }
+        
+        return false;
+    }
+};
