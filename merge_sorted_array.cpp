5612c916ad851b4b96ef7a0f7d5358bd54cc2cd3
tony <oy@lianlianke.com>
2013-09-26 15:11:47 +0800

    Merge Sorted Array AC.

---
+++ merge_sorted_array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int i, j, k;
+        
+        for (i = m - 1, j = n - 1, k = m + n - 1; i >= 0 && j >= 0; )
+        {
+            if (A[i] < B[j]) A[k--] = B[j--];
+            else A[k--] = A[i--];
+        }
+        while (j >= 0) A[k--] = B[j--];
+    }
+};
