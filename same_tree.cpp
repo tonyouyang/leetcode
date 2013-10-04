98b49f07271193196789be3e9ca693be1f3884f5
tony <oy@lianlianke.com>
2013-10-04 12:59:57 +0800

    simpler code

--- same_tree.cpp
+++ same_tree.cpp
@@ -1,31 +1,17 @@
-// Given two binary trees, write a function to check if they are equal or not.
-
-// Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
-
-using namespace std;
-
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-class Solution
-{
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
 public:
-    bool isSameTree(TreeNode *p, TreeNode *q)
-    {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (p == NULL && q == NULL) return true;
-        if (p == NULL || q == NULL) return false;
-        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (p == NULL || q == NULL) return p == q;
+        else return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
-
-int main(int argc, char const *argv[])
-{
-    return 0;
-}
