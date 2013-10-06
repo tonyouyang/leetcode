db31a71935aa81390be6856dc656f8c8a02942cb
tony <oy@lianlianke.com>
2013-10-06 22:53:40 +0800

    better redundant

--- word_break_ii.cpp
+++ word_break_ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<string> ret;
-    bool wordBreak(string s, unordered_set<string> &dict, unordered_set<string> &miss, string current) {
+    bool wordBreak(string s, int start, unordered_set<string> &dict, vector<bool> &miss, string current) {
         bool can_break = false;
         
-        for (int i = 0; i < s.size(); ++i)
+        for (int i = 1; start + i - 1 < s.size(); ++i)
         {
-            string prefix = s.substr(0, i + 1);
+            string prefix = s.substr(start, i);
             
             if (dict.find(prefix) != dict.end()) {
-                if (i == s.size() - 1) {
+                if (start + i == s.size()) {
                     ret.push_back(current + prefix);
                     return true;
                 }
                 else
                 {
-                    string suffix = s.substr(i + 1);
-                    
-                    if (miss.find(suffix) == miss.end())
+                    // 0 ... start .... start + i - 1, start + i ... size - 1
+                    if (!miss[start + i])
                     {
-                        if (wordBreak(suffix, dict, miss, current + prefix + " ")) can_break = true;
-                        else miss.insert(suffix);
+                        string suffix = s.substr(i + 1);
+                        
+                        if (wordBreak(s, start + i, dict, miss, current + prefix + " ")) can_break = true;
+                        else miss[start + i] = true;
                     }
                 }
             }
@@ -31,8 +32,8 @@ public:
     vector<string> wordBreak(string s, unordered_set<string> &dict) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         ret.clear();
-        unordered_set<string> miss;
-        wordBreak(s, dict, miss, "");
+        vector<bool> miss(s.size(), false);
+        wordBreak(s, 0, dict, miss, "");
         return ret;
     }
 };
